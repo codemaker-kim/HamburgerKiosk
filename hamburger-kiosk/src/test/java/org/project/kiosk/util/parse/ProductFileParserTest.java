3730577913fb1429f05c7983c4dff4b58c1092c8
codemaker-kim <tnals72441@daum.net>
2025-03-03 21:17:21 +0900

    Test: "상품 파일 불러오는 테스트 작성"

---
+++ hamburger-kiosk/src/test/java/org/project/kiosk/util/parse/ProductFileParserTest.java
@@ -0,0 +1,44 @@
+package org.project.kiosk.util.parse;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.project.kiosk.product.domain.Category;
+import org.project.kiosk.product.domain.Product;
+
+import java.io.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
+
+class ProductFileParserTest {
+    private ProductFileParser parser;
+
+    @Test
+    @DisplayName("정상적인 파일을 받았을 때 파일을 성공적으로 파싱하여 리스트로 반환한다.")
+    void parserTest1() throws IOException {
+        //given
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("test.txt"));
+        ProductFileParser parser = new ProductFileParser();
+        bos.write("name,price,quantity,description,category\n".getBytes());
+        bos.write("치킨버거,7000,15,\"치킨으로 만든 햄버거\",햄버거\n".getBytes());
+        bos.write("불고기버거,5000,6,\"불고기로 만든 햄버거\",햄버거\n".getBytes());
+        bos.flush();
+
+        File file = new File("test.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        //when
+        List<Product> productList = parser.parseToProductList(reader);
+        reader.close();
+        file.delete();
+
+        //then
+        assertThat(productList)
+                .extracting("name", "price", "stockQuantity", "description", "category")
+                .containsExactlyInAnyOrder(
+                        tuple("치킨버거", 7000, 15, "치킨으로 만든 햄버거", Category.BURGER),
+                        tuple("불고기버거",5000,6,"불고기로 만든 햄버거", Category.BURGER)
+                );
+    }
+}
