95a7016a19ca0fcbe2bca5dd6d0fe3389c564a8a
codemaker-kim <tnals72441@daum.net>
2025-03-03 21:31:17 +0900

    Chore: "상품 저장 클래스 역할에 맞게 이름 변경"

--- hamburger-kiosk/src/main/java/org/project/kiosk/product/storage/ProductStorage.java
+++ hamburger-kiosk/src/main/java/org/project/kiosk/product/storage/ProductStorage.java
@@ -4,10 +4,10 @@ import org.project.kiosk.product.domain.Product;
 
 import java.util.List;
 
-public class ProductFileStorage {
+public class ProductStorage {
     private final List<Product> products;
 
-    public ProductFileStorage(List<Product> products) {
+    public ProductStorage(List<Product> products) {
         this.products = products;
     }
 }
